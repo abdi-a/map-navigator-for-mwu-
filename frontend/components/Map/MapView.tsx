2a9e533aaa59ad88c4f02f3e9b9df2d48d3de6d5
Abdisa Awel <abdisaawel313@gmail.com>
2026-02-04 05:30:38 -0800

    Set Satellite as default base layer
    Move the 'checked' prop from the Street Map base layer to the Satellite base layer so the map initially loads with satellite imagery by default. No other functional changes were made to the TileLayer configurations.

--- frontend/components/Map/MapView.tsx
+++ frontend/components/Map/MapView.tsx
@@ -67,14 +67,14 @@ export default function MapView({ buildings, selectedBuilding, onMarkerClick, ca
       maxZoom={19}
     >
       <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="Street Map">
+        <LayersControl.BaseLayer name="Street Map">
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
         </LayersControl.BaseLayer>
 
-        <LayersControl.BaseLayer name="Satellite">
+        <LayersControl.BaseLayer checked name="Satellite">
           <TileLayer
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
